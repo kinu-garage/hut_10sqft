6b254cc2b5e1ca8b9462837ea6718448607bcac0
Isaac IY Saito <130s@lateeye.net>
2014-06-29 03:13:32 +0900

    add bashrc, emacs setting

--- install_ubuntu_common.bash
+++ install_ubuntu_common.bash
@@ -1,15 +1,36 @@
 #!/bin/bash
 
+function show_usage {
+    echo >&2 "usage: $0 [user accout (default:n130s)]"
+    echo >&2 " [-h|--help] print this message"
+    exit 0
+}
+
+# command line parse
+OPT=`getopt -o h -l help -- $*`
+if [ $? != 0 ]; then
+    # If no arg, run show_usage function.
+    show_usage
+fi
+
 DIST_TRUSTY="Trusty"
 DISTRO=$DIST_TRUSTY
 
-#sudo apt-get update
+sudo apt-get update
 
 PKG_TO_INSTALL=""
 
+# Setup terminal
+cd ~
+wget https://raw.githubusercontent.com/130s/compenv_ubuntu/master/dot_bashrc_default .bashrc
+
+# Setup emacs
+cd ~
+wget https://raw.githubusercontent.com/130s/compenv_ubuntu/master/dot_emacs_default .emacs
+
 # For Japanese input.
 ##TODO if DISTRO < Saucy
-PKG_JP_INPUT="ibus mozc-server emacs-mozc"
+PKG_JP_INPUT="ibus ibus-el mozc-server emacs-mozc"
 PKG_TO_INSTALL="$PKG_TO_INSTALL $PKG_JP_INPUT"
 ##TODO if Saucy <= DISTRO install fcitx
 
@@ -28,6 +49,5 @@ PKG_TO_INSTALL="$PKG_TO_INSTALL $PKG_RANDOM_TOOLS"
 echo Installing $PKG_TO_INSTALL
 sudo apt-get install -y $PKG_TO_INSTALL
 
-# Setup terminal
 
-# Setup emacs
+
