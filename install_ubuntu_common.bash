de8ed9584269d1b4c4b1077136a8b74a4e740954
Isaac IY Saito <130s@lateeye.net>
2014-09-22 06:30:06 -0500

    Add indicator-multiload, tree

--- install_ubuntu_common.bash
+++ install_ubuntu_common.bash
@@ -43,11 +43,8 @@ wget https://raw.githubusercontent.com/130s/compenv_ubuntu/master/dot_gitconfig 
 wget https://raw.githubusercontent.com/130s/compenv_ubuntu/master/dot_gitignore_global && mv dot_gitignore_global .gitignore_global
 
 # Random tools
-PKG_RANDOM_TOOLS="psensor ack-grep googleearth-package sysinfo synaptic"
+PKG_RANDOM_TOOLS="psensor indicator-multiload ack-grep googleearth-package sysinfo synaptic tree"
 PKG_TO_INSTALL="$PKG_TO_INSTALL $PKG_RANDOM_TOOLS"
 
 echo Installing $PKG_TO_INSTALL
 sudo apt-get install -y $PKG_TO_INSTALL
-
-
-
