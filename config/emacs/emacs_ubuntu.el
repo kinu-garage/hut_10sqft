dd2c0feefc3e59c39096c06242a58010cf88c248
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-06-10 14:31:00 -0700

    [emacs] Toggle input method with C-q from now on.

--- config/emacs/emacs_ubuntu.el
+++ config/emacs/emacs_ubuntu.el
@@ -7,13 +7,10 @@
 ; 2/15/2016 Without this, encoding may not be saved proerply?
 (set-language-environment "Japanese")
 (setq default-input-method "japanese-mozc")
-;;;(define-key mozc-mode-map "$" 'YaTeX-insert-dollar-or-mozc-insert)
-(define-key mozc-mode-map "\C-\o" 'YaTeX-insert-dollar-or-mozc-insert)
-(defun YaTeX-insert-dollar-or-mozc-insert ()
-  (interactive)
-  (if (eq major-mode 'yatex-mode)
-      (YaTeX-insert-dollar)
-    (mozc-insert)))
+;;;(global-set-key (kbd "\C-o") 'toggle-input-method)  ; This doesn't seem to be working. Probably collides with OS' input key that is also \C-o ?
+(global-set-key (kbd "\C-q") 'toggle-input-method)
+; 20160609 Not sure how effective this is but I just leave it. https://wiki.archlinuxjp.org/index.php/Mozc#Emacs_.E3.81.A7_Mozc_.E3.82.92.E4.BD.BF.E3.81.86
+(setq mozc-candidate-style 'overlay)
 
 ; 6/30/2012/http://superuser.com/questions/165278/copying-text-from-emacs-into-other-programs
 (setq x-select-enable-clipboard t)      ;Make kill/yank work with the X clipboard
