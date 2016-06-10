1bbc3ea41830f2bab6a77b1881ed561c13d19abf
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-06-10 14:50:00 -0700

    [ros] do not source ros setup script if file not exist (fix #15).

--- config/ros/setup_ros_tork-kudu1.bash
+++ config/ros/setup_ros_tork-kudu1.bash
@@ -2,7 +2,10 @@ DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # setup for ROS common for all 130s machines.
 source $DIR_THIS/setup_ros_common.bash
 
-source ~/link/ROS/indigo_trusty/setup_ros_dist.bash
-#source ~/link/ROS/jade_trusty/setup_ros_dist.bash
+DISTRO_ROS_LINUX=indigo_trusty
+
+if [ -e ~/link/ROS/$DISTRO_ROS_LINUX/setup_ros_dist.bash ]; then
+  source ~/link/ROS/$DISTRO_ROS_LINUX/setup_ros_dist.bash
+fi
 
 export CMAKE_ECLIPSE_VERSION=4.4 # Eclipse Lunar
