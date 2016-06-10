8010e76cca4afcbaa405ddf38237600ecded6de9
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-06-10 14:10:25 -0700

    [emacs] quick fix for mozc for 14.04 and 15.04.

--- config/emacs/emacs_ubuntu.el
+++ config/emacs/emacs_ubuntu.el
@@ -1,26 +1,11 @@
 ;; 6/9/2016 Common config read here.
 (load "~/link/github_repos/130s/compenv_ubuntu/config/emacs/emacs.el")
 
-;; 9/16/2011 For Mozc + ibus
-;; Turn on ibus-mode automatically after loading .emacs
-(require 'ibus)  ;; This requires ibus-el to be installed (available via apt)
-(add-hook 'after-init-hook 'ibus-mode-on)
-;; Use C-SPC for Set Mark command
-(ibus-define-common-key ?\C-\s nil)
-;; Use C-/ for Undo command
-;(ibus-define-common-key ?\C-/ nil)
-;; Change cursor color depending on IBus status
-(setq ibus-cursor-color '("limegreen" "white" "blue"))
-(global-set-key "\C-\o" 'ibus-toggle)
-
-;; mozc
-;; 2016/5/12 moved from the downstream emacs_tork-kudu1.el
-;(require 'mozc)
-;(setq default-input-method "japanese-mozc")
-
 ;;20160429 Moved from downstream (kudu1, Trusty 14.04), hoping this is valid for all Ubuntu machines.
 ;; mozc
 (require 'mozc)
+; 2/15/2016 Without this, encoding may not be saved proerply?
+(set-language-environment "Japanese")
 (setq default-input-method "japanese-mozc")
 ;;;(define-key mozc-mode-map "$" 'YaTeX-insert-dollar-or-mozc-insert)
 (define-key mozc-mode-map "\C-\o" 'YaTeX-insert-dollar-or-mozc-insert)
@@ -33,6 +18,3 @@
 ; 6/30/2012/http://superuser.com/questions/165278/copying-text-from-emacs-into-other-programs
 (setq x-select-enable-clipboard t)      ;Make kill/yank work with the X clipboard
 (setq interprogram-paste-function 'x-cut-buffer-or-selection-value)
-
-; 2/15/2016 Without this, encoding may not be saved proerply?
-(set-language-environment "Japanese")
--- config/emacs/emacs_ubuntu_trusty.el
+++ config/emacs/emacs_ubuntu_trusty.el
@@ -2,10 +2,5 @@
 
 (load "~/link/github_repos/130s/compenv_ubuntu/config/emacs/emacs_ubuntu.el")
 
-; 3/5/2015 for ibus
-(require 'ibus)
-(add-hook 'after-init-hook 'ibus-mode-on)
-(add-hook 'after-init-hook 'ibus-mode)
-
 ;; Custom ROS location
 (set-register ?r '(file . "~/link/ROS/indigo_trusty/"))
