c170b157111da6bad23972fe2020627e3f465283
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-08-09 16:54:17 -0700

    [bash] Fix #72.

--- config/bash/rc_t440s.bash
+++ config/bash/rc_t440s.bash
@@ -8,4 +8,4 @@ export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64/jre"  # By Amazon EC2
 
 # 20160716 git ssh issue https://github.com/130s/compenv_ubuntu/issues/64
 # Added in https://github.com/130s/compenv_ubuntu/pull/65
-if [ -f ~/.ssh/id_rsa]; then ssh-add ~/.ssh/id_rsa_130s-t440s; fi  # Key is for github
+if [ -f ~/.ssh/id_rsa ]; then ssh-add ~/.ssh/id_rsa_130s-t440s; fi  # Key is for github
