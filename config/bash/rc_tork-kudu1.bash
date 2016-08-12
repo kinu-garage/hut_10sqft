8bae42551533c06846985dd2d79565eafe87b8dd
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-08-12 14:11:13 -0700

    [bash] Fix kudu1 conf.

--- config/bash/rc_tork-kudu1.bash
+++ config/bash/rc_tork-kudu1.bash
@@ -5,4 +5,4 @@ source $DIR_THIS/../ros/setup_ros_tork-kudu1.bash
 
 # 20160716 git ssh issue https://github.com/130s/compenv_ubuntu/issues/64
 # Added in https://github.com/130s/compenv_ubuntu/pull/65
-if [ -f ~/.ssh/id_rsa]; then ssh-add ~/.ssh/id_rsa; fi  # Key is for github
+if [ -f ~/.ssh/id_rsa ]; then ssh-add ~/.ssh/id_rsa; fi  # Key is for github
