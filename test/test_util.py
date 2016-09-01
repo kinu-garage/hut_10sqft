a45a89b3f211772a6b5736134edea196cab14e59
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2016-08-31 17:15:50 -0700

    [util] Add in-file string replacement command.

---
+++ test/test_util.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import unittest
+import urllib
+
+from util import Util
+
+
+class TestUtil(unittest.TestCase):
+
+    def test_replace_str_infile(self):
+        os.chdir('/tmp')
+        # Prepare files to be manipulated.
+        FILE_STRTEST = 'https://raw.githubusercontent.com/ros-planning/moveit/kinetic-devel/moveit/package.xml'
+        FILENAME_STRTEST = 'file_strtest.txt'
+        MATCH_STR_REGEX = '<version>.*</version>'
+        NEW_STR = '<version>100000000000</version>'  # Something unrealistically used by anyone in any software.
+        testfile = urllib.URLopener()
+        # Save temporarily. This method is intended for files on the filesystem.
+        testfile.retrieve(FILE_STRTEST, FILENAME_STRTEST)         
+
+        # TODO Run the process
+        # Find all package.xml files in sub-folders.
+        files_found = Util.find_all(FILENAME_STRTEST, '/tmp')
+        for f in files_found:
+            print(f)
+    #        replace(f, "<version>.*</version>", "<version>0.8.2</version>")
+            util.replace(f, MATCH_STR_REGEX, NEW_STR)
+
+        # Assert if the new str is contained and old str are not.
+        is_replaced = False
+        file_strtest = open(FILENAME_STRTEST).read()
+        print('Content of the file tested: '.format(file_strtest))
+        if NEW_STR in file_strtest:
+            is_replaced = True        
+        self.assertTrue(is_replaced, 'String is not found in the targeted file.')
---
+++ util/util.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Copyright 2016 Isaac I. Y. Saito.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import fileinput
+import os
+import re
+import sys
+
+class Util():
+
+    @staticmethod
+    def find_all(filename, path):
+        '''
+        http://stackoverflow.com/questions/1724693/find-a-file-in-python
+        @param filename: (str) Full file name to be found.
+        @param path: (str) Top level path to search.
+        @return: List of absolute path of the files.
+        '''
+        result = []
+        for root, dirs, files in os.walk(path):
+            if filename in files:
+                result.append(os.path.join(root, filename))
+        return result
+
+    @staticmethod
+    def replaceAll(file, searchExp, replaceExp):
+        '''
+        http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
+    
+        Example usage: replaceAll("/fooBar.txt","Hello\sWorld!$","Goodbye\sWorld.")
+        '''
+        for line in fileinput.input(file, inplace=1):
+            if searchExp in line:
+                line = line.replace(searchExp, replaceExp)
+            sys.stdout.write(line)
+    
+    def replace(filename, pattern, subst):
+        '''
+        http://stackoverflow.com/a/13641746/577001
+        RegEx capable.
+        '''
+    
+        # Read contents from filename as a single string
+        file_handle = open(filename, 'r')
+        file_string = file_handle.read()
+        file_handle.close()
+    
+        # Use RE package to allow for replacement (also allowing for (multiline) REGEX)
+        file_string = (re.sub(pattern, subst, file_string))
+    
+        # Write contents to file.
+        # Using mode 'w' truncates the file.
+        file_handle = open(filename, 'w')
+        file_handle.write(file_string)
+        file_handle.close()
