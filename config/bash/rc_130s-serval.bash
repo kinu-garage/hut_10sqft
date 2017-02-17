1500a21b83b1e9037df73ce096ac5ee915c90af1
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-02-17 03:56:28 -0800

    [ROS][Serval] Fix init process.

--- config/bash/rc_130s-serval.bash
+++ config/bash/rc_130s-serval.bash
@@ -1,6 +1,7 @@
 DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export DISTRO_ROS_LINUX=kinetic_xenial
+export CMAKE_ECLIPSE_VERSION=4.4 # Eclipse Luner
+export DISTRO_ROS_LINUX=kinetic  # Folder name of ROS work spaces at ~/link/ROS/
 
 source $DIR_THIS/ubuntu_common.bash
 
