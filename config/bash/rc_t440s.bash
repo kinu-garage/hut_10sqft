c1897ad71bd2de006c2d7adefe1155c022252f72
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-06-20 14:49:02 -0700

    [t440s] Upgraded to Ubuntu Xenial.

--- config/bash/rc_t440s.bash
+++ config/bash/rc_t440s.bash
@@ -1,5 +1,7 @@
 DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+export DISTRO_ROS_LINUX=kinetic  # Folder name of ROS work spaces at ~/link/ROS/
+
 source $DIR_THIS/ubuntu_common.bash
 
 source $DIR_THIS/../ros/setup_ros_t440s.bash
