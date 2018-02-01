e84545d5a9dbb9908263475d92a8f3dce0cc6a02
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2018-02-01 12:02:08 -0800

    [ROS] Remove ROS_IP as it doesn't always work.

--- config/ros/setup_ros_130s-kudu1.bash
+++ config/ros/setup_ros_130s-kudu1.bash
@@ -3,4 +3,4 @@ DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export CMAKE_ECLIPSE_VERSION=4.4 # Eclipse Lunar
 
 # Requires the IP address statically assigned (by router/dhcp server etc.)
-export ROS_IP=192.168.0.15
+#export ROS_IP=192.168.0.15  # This gets tricky when a) multiple NICs are available b) dhcp isn't reliable.
