6b9ec0d85f3a59d953313fbd8b6d7edf172d5178
Isaac Saito <iisaac.saito@gmail.com>
2024-08-26 12:37:48 -0400

    Fix Ubuntu unintended paste with mouse' center key (address https://github.com/kinu-garage/hut_10sqft/issues/1077)
    Signed-off-by: Isaac Saito <iisaac.saito@gmail.com>

--- hut_10sqft/config/bash/ubuntu_common.bash
+++ hut_10sqft/config/bash/ubuntu_common.bash
@@ -1,4 +1,7 @@
 DIR_THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR_THIS/rc_debian.bash
 
+# https://askubuntu.com/a/1144039/24203 via https://github.com/kinu-garage/hut_10sqft/issues/1077
+xbindkeys -p
+
 source $DIR_THIS/../ros/setup_ros_ubuntu_common.bash  # This uses env var DISTRO_ROS_LINUX.
