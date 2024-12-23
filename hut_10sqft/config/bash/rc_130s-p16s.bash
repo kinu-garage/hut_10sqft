02af77a35a09246d8f42a24a56549ba90d7d9934
Isaac Saito <iisaac.saito@gmail.com>
2024-12-23 12:46:24 -0500

    bash; Add a path to Unreal Engine
    Signed-off-by: Isaac Saito <iisaac.saito@gmail.com>

--- hut_10sqft/config/bash/rc_130s-p16s.bash
+++ hut_10sqft/config/bash/rc_130s-p16s.bash
@@ -5,4 +5,5 @@ export DISTRO_ROS_LINUX=kinetic  # Folder name of ROS work spaces at ~/link/ROS/
 
 source $DIR_THIS/ubuntu_common.bash
 
-
+# 20241220 Unreal Engine https://github.com/kinu-garage/hut_10sqft/issues/861 this path is only available on p16s as of now.
+export PATH=~/pg/unreal-engine/UnrealEngine/Engine/Binaries/Linux/:$PATH
