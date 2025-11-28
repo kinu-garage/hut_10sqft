b261b059d5f6edf28cb6313f85cb3f40b84818ad
Isaac Saito <iisaac.saito@gmail.com>
2025-11-27 22:43:34 -0500

    0.4.0

--- hut_10sqft/setup.py
+++ hut_10sqft/setup.py
@@ -6,7 +6,7 @@ package_name = 'hut_10sqft'
 
 setup(
     name=package_name,
-    version='0.3.0',
+    version='0.4.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),     
